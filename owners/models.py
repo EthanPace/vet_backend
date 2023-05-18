8d975fa9219c34db0cf4efe4072c93711a473858
Hamish <hstockwell151@gmail.com>
2023-05-18 16:50:12 +1000

    Fixed the show/get method under owners

--- owners/models.py
+++ owners/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 class Owner(models.Model):
-	id = models.AutoField(primary_key=True)
-	first_name = models.CharField(max_length=255)
-	last_name = models.CharField(max_length=255)
-	email = models.CharField(max_length=255)
-	phone_number = models.CharField(max_length=255)
-	available_times = models.CharField(max_length=255)
-	def __to_lib__():
-		return {
-			"id": self.id,
-			"first_name": self.first_name,
-			"last_name": self.last_name,
-			"email": self.email,
-			"phone_number": self.phone_number,
-			"available_times": self.available_times,
-		}
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    available_times = models.CharField(max_length=255)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "available_times": self.available_times,
+        }
