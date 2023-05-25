335cf61e2df2efa177d77033ecf50667f9e39b4b
Ethan Pace <ethanpace212@gmail.com>
2023-05-25 11:12:56 +1000

    added slashes to add endpoints

--- owners/urls.py
+++ owners/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('details/<int:id>', views.details, name='details'),
-    path('add', views.add, name='add'),
+    path('add/', views.add, name='add'),
     path('edit/<int:id>', views.edit, name='edit'),
     path('delete/<int:id>', views.delete, name='delete'),
 ]
