0ea5fb2a672874441db51838fb588fef8eb77b99
Ethan Pace <ethanpace212@gmail.com>
2023-05-25 11:35:27 +1000

    tested all views

--- login/views.py
+++ login/views.py
@@ -1,13 +1,11 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
-from .serializers import UserSerializer
 from json import loads
 from hashlib import sha256
 # Login
 # Takes a username and password as part of the request body
 # Returns the id and role of the user with the given username and password
-# TODO: Add Authorization
 @csrf_exempt
 def login(request):
 	if request.method == 'POST':
@@ -39,7 +37,6 @@ def logout(request):
 # Add functionality for users
 # Takes a username and password as part of the request body
 # Returns the id and role of the newly created user
-# TODO: Test this
 @csrf_exempt
 def register(request):
 	if request.method == 'POST':
@@ -48,7 +45,7 @@ def register(request):
 		if user:
 			return JsonResponse({'error': 'A user with that username already exists.'}, status=409)
 		else:
-			user = User.objects.create(username=data['username'], password=hash(data['password']))
+			user = User.objects.create(username=data['username'], password=hash(data['password']), role=data['role'])
 			return JsonResponse({"result":"success", "data":{'id': user.id,'name':user.username, 'role': user.role}}, status=201)
 	else:
 		return JsonResponse({'error': 'This endpoint only accepts POST requests.'}, status=405)
@@ -56,15 +53,14 @@ def register(request):
 # Update functionality for users
 # Takes an id as part of the endpoint and all user fields as part of the request body
 # Returns the id and role of the updated user
-# TODO: Test this
 @csrf_exempt
 def edit(request, id):
 	if request.method == 'POST':
 		data = loads(request.body)
 		user = User.objects.filter(id=id)
 		if user:
-			user.update(username=data['username'], password=hash(data['password']))
-			return JsonResponse({"result":"success","data":{'id': user[0].id, 'role': user[0].role}}, safe=False, status=200)
+			user.update(username=data['username'], password=hash(data['password']), role=data['role'])
+			return JsonResponse({"result":"success","data":{'id': user[0].id, 'role': user[0].role, "hashcheckREMOVE":user[0].password}}, safe=False, status=200)
 		else:
 			return JsonResponse({'error': 'No user found with that id.'}, status=400)
 	else:
@@ -73,7 +69,6 @@ def edit(request, id):
 # Delete functionality for users
 # Takes an id as part of the endpoint
 # Returns a success message if the user was deleted successfully
-# TODO: Test this
 @csrf_exempt
 def delete(request, id):
 	if request.method == 'POST':
@@ -89,6 +84,5 @@ def delete(request, id):
 # Hash functionality for passwords
 # Takes a password as part of the request body
 # Returns the hashed password
-# TODO: Test this
 def hash(password):
 	return sha256(password.encode('utf-8')).hexdigest()
