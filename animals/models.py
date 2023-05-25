b98c04d5bcd207341a751269d48e33778ed27dc2
Ethan Pace <ethanpace212@gmail.com>
2023-05-25 11:12:27 +1000

    fixed date in animals model and commented

--- animals/models.py
+++ animals/models.py
@@ -10,14 +10,13 @@ class Animal(models.Model):
 	breed = models.CharField(max_length=50)
 	colour = models.CharField(max_length=50)
 	sex = models.CharField(max_length=50)
-	#DOB = models.DateField()
+	DOB = models.DateField()
 	weight = models.IntegerField()
 
 	vaccination_status = models.CharField(max_length=255)
-	#last_vaccination_date = models.DateField()
-	#next_vaccination_date = models.DateField()
-	
-	#desexed = models.BooleanField(default=False)
+	last_vaccination_date = models.DateField()
+	next_vaccination_date = models.DateField()
+
 	microchip_number = models.IntegerField()
 
 	def __str__(self):
--- animals/views.py
+++ animals/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 from .models import Animal
 from .serializers import AnimalSerializer
 from json import loads
@@ -11,16 +12,24 @@ from owners.models import Owner
 # TODO: Add Authorization
 # TODO: Test this
 def index(request):
+	# check if the request method is GET
 	if request.method == 'GET':
+		# check if the page and page_size parameters are in the request
 		if 'page' in request.GET:
+			# get the page and page_size parameters from the request
 			page = int(request.GET['page', 1])
 			page_size = int(request.GET['page_size', 10])
+			# get the animals for the given page
 			animals = Animal.objects.all()[(page - 1) * page_size:page * page_size]
 		else:
+			# get all animals
 			animals = Animal.objects.all()
+		# serialize the animals
 		serializer = AnimalSerializer(animals, many=True)
+		# return the overview of the full list of animals
 		return JsonResponse(overview(serializer.data), safe=False, status=200)
 	else:
+		# return an error if the request method is not GET
 		return JsonResponse({'error': 'This endpoint only accepts GET requests.'}, status=405)
 # Details
 # Takes an id as part of the endpoint
@@ -28,14 +37,21 @@ def index(request):
 # TODO: Add Authorization
 # TODO: Test this
 def details(request, id):
+	# check if the request method is GET
 	if request.method == 'GET':
+		# find the animal with the given id
 		animal = Animal.objects.filter(id=id)
+		# check if the animal exists
 		if animal:
+			# serialize the animal (not entirely sure why this is necessary)
 			serializer = AnimalSerializer(animal[0])
+			# return the serialized animal data
 			return JsonResponse(find_visits(serializer.data), safe=False, status=200)
 		else:
+			# return an error if the animal doesn't exist
 			return JsonResponse({'error': 'No animal found with that id.'}, status=400)
 	else:
+		# return an error if the request method is not GET
 		return JsonResponse({'error': 'This endpoint only accepts GET requests.'}, status=405)
 # Add
 # Create functionality for animals
@@ -46,32 +62,63 @@ def details(request, id):
 @csrf_exempt
 def add(request):
 	if request.method == 'POST':
+		# get the request body and load as json
 		body = request.body.decode('utf-8')
 		data = loads(body)
+		# check if the owner exists
 		if find_owner(data['owner']):
+			# convert dates to datetime objects
+			data['DOB'] = datetime.strptime(data['DOB'], '%d-%m-%Y').date()
+			# check if the last vaccination date exists, and convert to datetime object if it does
+			if data['last_vaccination_date']:
+				data['last_vaccination_date'] = datetime.strptime(data['last_vaccination_date'], '%d-%m-%Y').date()
+			# check if the next vaccination date exists, and convert to datetime object if it does
+			if data['next_vaccination_date']:
+				data['next_vaccination_date'] = datetime.strptime(data['next_vaccination_date'], '%d-%m-%Y').date()
+			# serialize the data
 			serial = AnimalSerializer(data=data)
+			# check if the data is valid
 			if serial.is_valid():
+				# save the data
 				serial.save()
+				# return the result
 				return JsonResponse({"result":"success", "data":serial.data}, safe=False, status=201)
 			else:
+				# return an error if the data is invalid
 				return JsonResponse({'error': 'Invalid data.'}, status=400)
 		else:
+			# return an error if the owner doesn't exist
 			return JsonResponse({'error': 'No owner found with that id.'}, status=400)
 	else:
+		# return an error if the request method is not POST
 		return JsonResponse({'error': 'This endpoint only accepts POST requests.'}, status=405)
 # Edit
 # Update functionality for animals
 # Takes an id as part of the endpoint and all animal fields as part of the request body
-# Returns the id of the updated animal\
+# Returns the id of the updated animal
 # TODO: Add Authorization
 # TODO: Test this
 @csrf_exempt
 def edit(request, id):
+	# check if the request method is PUT
 	if request.method == 'PUT':
+		# get the request body and load as json
 		data = loads(request.body)
+		# check if the owner exists
 		if find_owner(data['owner']):
+			# convert dates to datetime objects
+			data['DOB'] = datetime.strptime(data['DOB'], '%d-%m-%Y').date()
+			# check if the last vaccination date exists, and convert to datetime object if it does
+			if data['last_vaccination_date']:
+				data['last_vaccination_date'] = datetime.strptime(data['last_vaccination_date'], '%d-%m-%Y').date()
+			# check if the next vaccination date exists, and convert to datetime object if it does
+			if data['next_vaccination_date']:
+				data['next_vaccination_date'] = datetime.strptime(data['next_vaccination_date'], '%d-%m-%Y').date()
+			# find the animal with the given id
 			animal = Animal.objects.filter(id=id)
+			# check if the animal exists
 			if animal:
+				# update the animal with the given data
 				animal.update(
 					name=data['name'],
 					owner=data['owner'],
@@ -87,12 +134,16 @@ def edit(request, id):
 					desexed=data['desexed'],
 					microchip_number=data['microchip_number']
 				)
+				# return the id of the updated animal with a success message
 				return JsonResponse({"result":"success", 'id': animal[0].id}, safe=False, status=200)
 			else:
+				# return an error if the animal doesn't exist
 				return JsonResponse({'error': 'No animal found with that id.'}, status=400)
 		else:
+			# return an error if the owner doesn't exist
 			return JsonResponse({'error': 'No owner found with that id.'}, status=400)
 	else:
+		# return an error if the request method is not PUT
 		return JsonResponse({'error': 'This endpoint only accepts PUT requests.'}, status=405)
 # Delete
 # Delete functionality for animals
@@ -102,43 +153,59 @@ def edit(request, id):
 # TODO: Test this
 @csrf_exempt
 def delete(request, id):
+	# check if the request method is DELETE
 	if request.method == 'DELETE':
+		# find the animal with the given id
 		animal = Animal.objects.filter(id=id)
+		# check if the animal exists
 		if animal:
+			# delete the animal
 			animal.delete()
+			# return a success message
 			return JsonResponse({'success': 'Animal deleted successfully.'}, status=200)
 		else:
+			# return an error if the animal doesn't exist
 			return JsonResponse({'error': 'No animal found with that id.'}, status=400)
 	else:
+		# return an error if the request method is not DELETE
 		return JsonResponse({'error': 'This endpoint only accepts DELETE requests.'}, status=405)
 # Overview
 # Takes a list of animals
 # Returns an overview of all animals
 # TODO: Check with Maclane to see what he needs here
 def overview(animals):
+	# create an empty list
 	overview = []
+	# loop through each animal
 	for animal in animals:
+		# append the animal's id, name, and species to the list
 		overview.append({
 			"id":animal['id'],
 			"name":animal['name'], 
 			"species":animal['species']
 		})
+	# return the list
 	return overview
 # Find Visits
 # Takes an animal
 # Returns a list of visits for that animal
 # TODO: Test this
 def find_visits(animal):
+	# get all visits for the animal
 	visits = Visit.objects.filter(animal=animal['id'])
+	# create an empty list
 	animal['visits'] = []
+	# loop through each visit
 	for visit in visits:
+		# append the visit's id and date_time to the list
 		animal['visits'].append({
 			"id":visit.id,
 			"date_time":visit.date_time,
 		})
+	# return the animal with the visits added
 	return animal
 # Find Owner
 # Takes an owner id
 # Returns the owner with that id
 def find_owner(id):
-	return Owner.objects.filter(id=id)[0]
+	return Owner.objects.filter(id=id)
