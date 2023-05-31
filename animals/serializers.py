592cc0f100810f52d43d7624d948060cbf14b7c4
Ethan Pace <ethanpace212@gmail.com>
2023-05-31 13:38:49 +1000

    empty

--- animals/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Animal
-
-class AnimalSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Animal
-		#fields = ['id', 'name', 'species', 'breed', 'age', 'colour', 'weight', 'description', 'image_url', 'created_at', 'updated_at']
-		fields = ['id', 'name', 'age', 'breed', 'species', 'colour', 'weight', 'DOB', 'vaccination_status', 'microchip_number', 'owner', 'created_at']
--- owners/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Owner
-
-class OwnerSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Owner
-		fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'available_times']
