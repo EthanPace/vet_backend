9fee2da939f0d7a06260ade5cc1eab9dc6d150fe
Ethan Pace <ethanpace212@gmail.com>
2023-06-09 14:47:05 +1000

    cors attempt 2

--- vet_backend/settings.py
+++ vet_backend/settings.py
@@ -29,7 +29,18 @@ ALLOWED_HOSTS = ['*']
 
 # CORS Settings
 
-CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ACCESS_CONTROL_ALLOW_HEADERS = [
     'accept',
